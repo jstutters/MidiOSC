32398ceba2d507f7e0cf35ef645b09e871143034
Jon Stutters <jstutters@jeremah.co.uk>
2016-03-20 15:17:25 +0000

    Fix a bunch of misc issues

--- main.cpp
+++ main.cpp
@@ -28,182 +28,186 @@
 #include "lo/lo.h"
 
 using namespace std;
-			
-void tokenize(const string& str, vector<string>& tokens, const string& delimiters = " ") {
-	// Skip delimiters at beginning.
-	string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-	// Find first "non-delimiter".
-	string::size_type pos     = str.find_first_of(delimiters, lastPos);
 
-	while (string::npos != pos || string::npos != lastPos)
-	{
-	    // Found a token, add it to the vector.
-	    tokens.push_back(str.substr(lastPos, pos - lastPos));
-	    // Skip delimiters.  Note the "not_of"
-	    lastPos = str.find_first_not_of(delimiters, pos);
-	    // Find next "non-delimiter"
-	    pos = str.find_first_of(delimiters, lastPos);
-	}
+void tokenize(const string& str, vector<string>& tokens, const string& delimiters = " ") {
+    // Skip delimiters at beginning.
+    string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+    // Find first "non-delimiter".
+    string::size_type pos     = str.find_first_of(delimiters, lastPos);
+
+    while (string::npos != pos || string::npos != lastPos)
+    {
+        // Found a token, add it to the vector.
+        tokens.push_back(str.substr(lastPos, pos - lastPos));
+        // Skip delimiters.  Note the "not_of"
+        lastPos = str.find_first_not_of(delimiters, pos);
+        // Find next "non-delimiter"
+        pos = str.find_first_of(delimiters, lastPos);
+    }
 }
 
 vector<int> selectPorts() {
-	int port;
-	vector<string>::iterator it;
-	string userInput;
-	vector<string>userInputTokenized;
-	vector<int> portList;
-	
-	cout << "Select ports: ";
-	getline(cin, userInput);
-	tokenize(userInput, userInputTokenized);
-	
-	for (it = userInputTokenized.begin(); it < userInputTokenized.end(); it++) {
-		stringstream(*it) >> port;
-		portList.push_back(port);
-	}
-	return portList;
+    int port;
+    vector<string>::iterator it;
+    string userInput;
+    vector<string>userInputTokenized;
+    vector<int> portList;
+
+    cout << "Select ports: ";
+    getline(cin, userInput);
+    tokenize(userInput, userInputTokenized);
+
+    for (it = userInputTokenized.begin(); it < userInputTokenized.end(); it++) {
+        stringstream(*it) >> port;
+        portList.push_back(port);
+    }
+    return portList;
 }
 
 void listInputPorts() {
-	RtMidiIn* midiIn = new RtMidiIn();
+    RtMidiIn* midiIn = new RtMidiIn();
     unsigned int nPorts = midiIn->getPortCount();
-	cout << "Found " << nPorts << " MIDI inputs." << endl;
-	string portName;
+    cout << "Found " << nPorts << " MIDI inputs." << endl;
+    string portName;
     if(nPorts == 0) {
         cout << "No ports available!\n";
     } else {
-		for(unsigned int i = 0; i < nPorts; i++) {
-			portName = midiIn->getPortName(i);
-			cout << "   Input port #" << i+1 << ": " << portName << endl;
-		}
-	}
-	delete midiIn;
+        for(unsigned int i = 0; i < nPorts; i++) {
+            portName = midiIn->getPortName(i);
+            cout << "   Input port #" << i+1 << ": " << portName << endl;
+        }
+    }
+    delete midiIn;
 }
 
 void listOutputPorts() {
-	RtMidiOut* midiOut = new RtMidiOut();
+    RtMidiOut* midiOut = new RtMidiOut();
     unsigned int nPorts = midiOut->getPortCount();
-	cout << "Found " << nPorts << " MIDI outputs." << endl;
-	string portName;
+    cout << "Found " << nPorts << " MIDI outputs." << endl;
+    string portName;
     if(nPorts == 0) {
         cout << "No ports available!\n";
     } else {
-		for(unsigned int i = 0; i < nPorts; i++) {
-			portName = midiOut->getPortName(i);
-			cout << "   Output port #" << i+1 << ": " << portName << endl;
-		}
-	}
-	delete midiOut;
+        for(unsigned int i = 0; i < nPorts; i++) {
+            portName = midiOut->getPortName(i);
+            cout << "   Output port #" << i+1 << ": " << portName << endl;
+        }
+    }
+    delete midiOut;
 }
 
 void error(int num, const char *msg, const char *path)
 {
-	cout << "liblo server error " << num << " in path " << path << ": " << msg << endl;
+    cout << "liblo server error " << num << " in path " << path << ": " << msg << endl;
 }
 
 map<string, int> populateMap() {
-	map<string, int> stringToStatus;
-	stringToStatus["note_off"] = 0x80;
-	stringToStatus["note_on"] = 0x90;
-	stringToStatus["key_pressure"] = 0xa0;
-	stringToStatus["controller_change"] = 0xb0;
-	stringToStatus["program_change"] = 0xc0;
-	stringToStatus["channel_pressure"] = 0xd0;
-	stringToStatus["pitch_bend"] = 0xe0;
-	stringToStatus["system_exclusive"] = 0xf0;
-	stringToStatus["song_position"] = 0xf2;
-	stringToStatus["song_select"] = 0xf3;
-	stringToStatus["tune_request"] = 0xf6;
-	stringToStatus["end_of_sysex"] = 0xf7;
-	stringToStatus["timing_tick"] = 0xf8;
-	stringToStatus["start_song"] = 0xfa;
-	stringToStatus["continue_song"] = 0xfb;
-	stringToStatus["stop_song"] = 0xfc;
-	return stringToStatus;
+    map<string, int> stringToStatus;
+    stringToStatus["note_off"] = 0x80;
+    stringToStatus["note_on"] = 0x90;
+    stringToStatus["key_pressure"] = 0xa0;
+    stringToStatus["controller_change"] = 0xb0;
+    stringToStatus["program_change"] = 0xc0;
+    stringToStatus["channel_pressure"] = 0xd0;
+    stringToStatus["pitch_bend"] = 0xe0;
+    stringToStatus["system_exclusive"] = 0xf0;
+    stringToStatus["song_position"] = 0xf2;
+    stringToStatus["song_select"] = 0xf3;
+    stringToStatus["tune_request"] = 0xf6;
+    stringToStatus["end_of_sysex"] = 0xf7;
+    stringToStatus["timing_tick"] = 0xf8;
+    stringToStatus["start_song"] = 0xfa;
+    stringToStatus["continue_song"] = 0xfb;
+    stringToStatus["stop_song"] = 0xfc;
+    return stringToStatus;
 }
 
 void stringReplace(string* str) {
-	replace_if(str->begin(), str->end(), bind2nd(equal_to<char>(), '_'), ' ');
+    replace_if(str->begin(), str->end(), bind2nd(equal_to<char>(), '_'), ' ');
 }
 
 int generic_handler(const char *path, const char *types, lo_arg **argv, int argc, void *data, void *user_data) {
-	vector<unsigned char> msg;
-	map<string, RtMidiOut*>::iterator it;
-	map<string, int> stringToStatus = populateMap();
-	
-	string pathString = path;
-	vector<string>pathTokenized;
-	tokenize(pathString, pathTokenized, "/");
-	if(pathTokenized[0].compare("midi") != 0) {
-		return 1;
-	}
-	
-	int channel = 0;
-	if(pathTokenized.size() == 3) {
-		string token = pathTokenized[2];
-		stringstream(token) >> channel;
-	}
-	string statusString = &argv[0]->s;
-	int status = stringToStatus[&argv[0]->s] + channel;
-	msg.push_back((unsigned char)status);
-	for(int i = 1; i < argc; i++) {
-		msg.push_back((unsigned char)argv[i]->i);
-	}
-	
-	stringReplace(&pathTokenized[1]);
-	
-	map<string, RtMidiOut*> *m = (map<string, RtMidiOut*>*)user_data;
-	it = m->find(pathTokenized[1]);
-	RtMidiOut* midi = it->second;
-	midi->sendMessage(&msg);
-	return 0;
+    vector<unsigned char> msg;
+    map<string, RtMidiOut*>::iterator it;
+    map<string, int> stringToStatus = populateMap();
+
+    string pathString = path;
+    vector<string>pathTokenized;
+    tokenize(pathString, pathTokenized, "/");
+    if(pathTokenized[0].compare("midi") != 0) {
+        return 1;
+    }
+
+    int channel = 0;
+    if(pathTokenized.size() == 3) {
+        string token = pathTokenized[2];
+        stringstream(token) >> channel;
+    }
+    string statusString = &argv[0]->s;
+    int status = stringToStatus[&argv[0]->s] + channel;
+    msg.push_back((unsigned char)status);
+    for(int i = 1; i < argc; i++) {
+        msg.push_back((unsigned char)argv[i]->i);
+    }
+
+    stringReplace(&pathTokenized[1]);
+
+    map<string, RtMidiOut*> *m = (map<string, RtMidiOut*>*)user_data;
+    it = m->find(pathTokenized[1]);
+    if(it == m->end()) {
+        cout << "error : midiout device not found: " << pathTokenized[1] << endl;
+        return 1;
+    }
+    RtMidiOut* midi = it->second;
+    midi->sendMessage(&msg);
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
-	vector<MidiInput*> inputs;
-	vector<int> portList;
-	vector<int>::iterator portIterator;
-	vector<MidiInput*>::iterator inputIterator;
-	
-	Options *opt = new Options();
-	opt->processArguments(argc, argv);
-	
-	listInputPorts();
-	portList = selectPorts();
+    vector<MidiInput*> inputs;
+    vector<int> portList;
+    vector<int>::iterator portIterator;
+    vector<MidiInput*>::iterator inputIterator;
 
-	for (portIterator = portList.begin(); portIterator < portList.end(); portIterator++) {
-		*portIterator -= 1;
-		MidiInput* m  = new MidiInput(*portIterator, opt->outputPort);
-		inputs.push_back(m);
-	}
-	
-	listOutputPorts();
-	portList = selectPorts();
+    Options *opt = new Options();
+    opt->processArguments(argc, argv);
 
-	map<string, RtMidiOut*> portMap;
-	RtMidiOut* midiOut = new RtMidiOut();
-	RtMidiOut* p;
-	for (portIterator = portList.begin(); portIterator < portList.end(); portIterator++) {
-		*portIterator -= 1;
-		p = new RtMidiOut();
-		p->openPort(*portIterator);
-		portMap[midiOut->getPortName(*portIterator)] = p;
-	}
-	
-	stringstream ss;
-	ss << opt->inputPort;
-	
-	lo_server_thread st = lo_server_thread_new(ss.str().c_str(), error);
-	lo_server_thread_add_method(st, NULL, NULL, generic_handler, &portMap);
-	lo_server_thread_start(st);
-	
-	delete opt;
-	
-    cout << "\nReading MIDI input ... press <enter> to quit.\n";
+    listInputPorts();
+    portList = selectPorts();
+
+    for (portIterator = portList.begin(); portIterator < portList.end(); portIterator++) {
+        *portIterator -= 1;
+        MidiInput* m  = new MidiInput(*portIterator, opt->outputPort);
+        inputs.push_back(m);
+    }
+
+    listOutputPorts();
+    portList = selectPorts();
+
+    map<string, RtMidiOut*> portMap;
+    RtMidiOut* midiOut = new RtMidiOut();
+    RtMidiOut* p;
+    for (portIterator = portList.begin(); portIterator < portList.end(); portIterator++) {
+        *portIterator -= 1;
+        p = new RtMidiOut();
+        p->openPort(*portIterator);
+        portMap[midiOut->getPortName(*portIterator)] = p;
+    }
+
+    stringstream ss;
+    ss << opt->inputPort;
+
+    lo_server_thread st = lo_server_thread_new(ss.str().c_str(), error);
+    lo_server_thread_add_method(st, NULL, NULL, generic_handler, &portMap);
+    lo_server_thread_start(st);
+
+    delete opt;
+
+    cout << endl << "Reading MIDI input ... press <enter> to quit." << endl;
     char input;
     cin.get(input);
 
-	for (inputIterator = inputs.begin(); inputIterator < inputs.end(); inputIterator++) {
-		delete *inputIterator;
-	}
+    for (inputIterator = inputs.begin(); inputIterator < inputs.end(); inputIterator++) {
+        delete *inputIterator;
+    }
 }
--- midiinput.cpp
+++ midiinput.cpp
@@ -19,181 +19,179 @@
 using namespace std;
 
 MidiInput::MidiInput(int p, int op) {
-	try {
-	    midiIn = new RtMidiIn();
-	} catch(RtMidiError &error) {
-		error.printMessage();
-	}
-	
-	string portName;
-	string* outputPort;
-	
-	stringstream ss;
-	ss << op;
-	outputPort = new string(ss.str());
-	
-	midiIn->openPort(p);
-	portName = midiIn->getPortName(p);
-	stringReplace(&portName, '_');
-	
-	threadData.portName = portName;
-	threadData.outputPort = *outputPort;
-    
+    try {
+        midiIn = new RtMidiIn();
+    } catch(RtMidiError &error) {
+        error.printMessage();
+    }
+
+    string portName;
+    string* outputPort;
+
+    stringstream ss;
+    ss << op;
+    outputPort = new string(ss.str());
+
+    midiIn->openPort(p);
+    portName = midiIn->getPortName(p);
+    stringReplace(&portName, '_');
+
+    threadData.portName = portName;
+    threadData.outputPort = *outputPort;
+
     midiIn->setCallback(onMidi, &threadData);
-	//respond to sysex and timing, ignore active sensing
+    //respond to sysex and timing, ignore active sensing
     midiIn->ignoreTypes(false, false, true);
-	cout << "Listening to port " << midiIn->getPortName(p) << endl;
-	
-	
+    cout << "Listening to port " << midiIn->getPortName(p) << endl;
 }
 
 MidiInput::~MidiInput() {
-	delete this->midiIn;
+    delete this->midiIn;
 }
 
 void MidiInput::stringReplace(string* str, char rep) {
-	replace_if(str->begin(), str->end(), bind2nd(equal_to<char>(), ' '), rep);
+    replace_if(str->begin(), str->end(), bind2nd(equal_to<char>(), ' '), rep);
 }
 
 void MidiInput::onMidi(double deltatime, vector<unsigned char> *message, void *userData) {
-	unsigned int nBytes = 0;
-	unsigned char channel = 0, status = 0;
+    unsigned int nBytes = 0;
+    unsigned char channel = 0, status = 0;
     string message_type;
-	int j = 0, bytes = 0;
-	
-	try {
-    	nBytes = message->size();
-	} catch(RtMidiError &error) {
-		error.printMessage();
-		exit(EXIT_FAILURE);
-	}
-	
-	if((message->at(0) & 0xf0) != 0xf0) {
-	    channel = message->at(0) & 0x0f;
-	    status = message->at(0) & 0xf0;
-	} else {
-		channel = 0;
-		status = message->at(0);
-	}
-	
-	MidiThreadData *data = static_cast<MidiThreadData*>(userData);
-	switch(status) {
-		case NOTE_OFF:
-		message_type = "note_off";
-		bytes = 2;
-		break;
-		
-		case NOTE_ON:
-		message_type = "note_on";
-		bytes = 2;
-		break;
-		
-		case KEY_PRESSURE:
-		message_type = "key_pressure";
-		bytes = 2;
-		break;
-		
-		case CONTROLLER_CHANGE:
-		message_type = "controller_change";
-		bytes = 2;
-		break;
-		
-		case PROGRAM_CHANGE:
-		message_type = "program_change";
-		bytes = 2;
-		break;
-		
-		case CHANNEL_PRESSURE:
-		message_type = "channel_pressure";
-		bytes = 2;
-		break;
-		
-		case PITCH_BEND:
-		message_type = "pitch_bend";
-		bytes = 2;
-		break;
-		
-		case SYSTEM_EXCLUSIVE:
-		if(message->size() == 6) {
-			unsigned int type = message->at(4);
-			if(type == 1) {
-				message_type = "mmc_stop";
-			} else if(type == 2) {
-				message_type = "mmc_play";
-			} else if(type == 4) {
-				message_type = "mmc_fast_forward";
-			} else if(type == 5) {
-				message_type = "mmc_rewind";
-			} else if(type == 6) {
-				message_type = "mmc_record";
-			} else if(type == 9) {
-				message_type = "mmc_pause";
-			}
-		}
-		bytes = 0;
-		break;
-		
-		case SONG_POSITION:
-		message_type = "song_position";
-		bytes = 2;
-		break;
-		
-		case SONG_SELECT:
-		message_type = "song_select";
-		bytes = 2;
-		break;
-		
-		case TUNE_REQUEST:
-		message_type = "tune_request";
-		bytes = 2;
-		break;
-		
-		case TIMING_TICK:
-		message_type = "timing_tick";
-		bytes = 0;
-		break;
-		
-		case START_SONG:
-		message_type = "start_song";
-		bytes = 0;
-		break;
-		
-		case CONTINUE_SONG:
-		message_type = "continue_song";
-		bytes = 0;
-		break;
-		
-		case STOP_SONG:
-		message_type = "stop_song";
-		bytes = 0;
-		break;
-		
-		default:
-		message_type = "";
-		bytes = 0;
-		break;
-	}
-	
-	if(status == NOTE_ON && message->at(2) == 0) {
-		message_type = "note_off";
-	}
+    int j = 0, bytes = 0;
 
-	lo_message m = lo_message_new();
-	lo_message_add_string(m, message_type.c_str());
-	
-	for(j = 0; j < bytes; j++) {
-		lo_message_add_int32(m, (int)message->at(j + 1));
-	}
+    try {
+        nBytes = message->size();
+    } catch(RtMidiError &error) {
+        error.printMessage();
+        exit(EXIT_FAILURE);
+    }
 
-	lo_address t = lo_address_new("239.0.0.1", data->outputPort.c_str());
-	stringstream path;
-	path << "/midi/" << data->portName;
-	if(bytes > 0) {
-		path << "/" << (int)channel;
-	}
-	string pathString;
-	pathString = path.str();
-	lo_send_message(t, pathString.c_str(), m);
-	lo_address_free(t);
-	lo_message_free(m);
+    if((message->at(0) & 0xf0) != 0xf0) {
+        channel = message->at(0) & 0x0f;
+        status = message->at(0) & 0xf0;
+    } else {
+        channel = 0;
+        status = message->at(0);
+    }
+
+    MidiThreadData *data = static_cast<MidiThreadData*>(userData);
+    switch(status) {
+        case NOTE_OFF:
+            message_type = "note_off";
+            bytes = 2;
+            break;
+
+        case NOTE_ON:
+            message_type = "note_on";
+            bytes = 2;
+            break;
+
+        case KEY_PRESSURE:
+            message_type = "key_pressure";
+            bytes = 2;
+            break;
+
+        case CONTROLLER_CHANGE:
+            message_type = "controller_change";
+            bytes = 2;
+            break;
+
+        case PROGRAM_CHANGE:
+            message_type = "program_change";
+            bytes = 2;
+            break;
+
+        case CHANNEL_PRESSURE:
+            message_type = "channel_pressure";
+            bytes = 2;
+            break;
+
+        case PITCH_BEND:
+            message_type = "pitch_bend";
+            bytes = 2;
+            break;
+
+        case SYSTEM_EXCLUSIVE:
+            if(message->size() == 6) {
+                unsigned int type = message->at(4);
+                if(type == 1) {
+                    message_type = "mmc_stop";
+                } else if(type == 2) {
+                    message_type = "mmc_play";
+                } else if(type == 4) {
+                    message_type = "mmc_fast_forward";
+                } else if(type == 5) {
+                    message_type = "mmc_rewind";
+                } else if(type == 6) {
+                    message_type = "mmc_record";
+                } else if(type == 9) {
+                    message_type = "mmc_pause";
+                }
+            }
+            bytes = 0;
+            break;
+
+        case SONG_POSITION:
+            message_type = "song_position";
+            bytes = 2;
+            break;
+
+        case SONG_SELECT:
+            message_type = "song_select";
+            bytes = 2;
+            break;
+
+        case TUNE_REQUEST:
+            message_type = "tune_request";
+            bytes = 2;
+            break;
+
+        case TIMING_TICK:
+            message_type = "timing_tick";
+            bytes = 0;
+            break;
+
+        case START_SONG:
+            message_type = "start_song";
+            bytes = 0;
+            break;
+
+        case CONTINUE_SONG:
+            message_type = "continue_song";
+            bytes = 0;
+            break;
+
+        case STOP_SONG:
+            message_type = "stop_song";
+            bytes = 0;
+            break;
+
+        default:
+            message_type = "";
+            bytes = 0;
+            break;
+    }
+
+    if(status == NOTE_ON && message->at(2) == 0) {
+        message_type = "note_off";
+    }
+
+    lo_message m = lo_message_new();
+    lo_message_add_string(m, message_type.c_str());
+
+    for(j = 0; j < bytes; j++) {
+        lo_message_add_int32(m, (int)message->at(j + 1));
+    }
+
+    lo_address t = lo_address_new("localhost", data->outputPort.c_str());
+    stringstream path;
+    path << "/midi/" << data->portName;
+    if(bytes > 0) {
+        path << "/" << (int)channel;
+    }
+    string pathString;
+    pathString = path.str();
+    lo_send_message(t, pathString.c_str(), m);
+    lo_address_free(t);
+    lo_message_free(m);
 }
